791940d964007513ddeff3fc3c0c1a1a72f10fd7
Timothy Lowe <timothylowe247@gmail.com>
2018-04-14 15:46:35 -0400

    async probs

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const sqlite = require("sqlite"),
   Sequelize = require("sequelize"),
   request = require("request"),
+  rp = require("request-promise"),
   express = require("express"),
   app = express(),
-  moment = require('moment');
+  axios = require('axios'),
+  moment = require("moment"),
+  recommendations = {};
 
 const {
   PORT = 3000,
@@ -20,18 +23,79 @@ Promise.resolve()
     if (NODE_ENV === "development") console.error(err.stack);
   });
 
-// ROUTES
-app.get("/films/:id/recommendations", getFilmRecommendations);
+//MODELS
 
-// const connection = new Sequelize('main', {
-//         dialect: 'sqlite',
-//          pool: {
-//                 max: 5,
-//                 min: 0,
-//                 idle: 10000
-//                   },
-//         storage: './db/database.db'
-//       });
+recommendations.currentFilm = (req, res, next) => {
+  const filmId = req.params.id;
+  sequelize
+    .query(`SELECT * FROM films WHERE films.id=${filmId}`)
+    .then(film => {
+      res.locals.currentFilm = film[0][0];
+      console.log("CURRENT FILM TEST", film);
+      next();
+    })
+    .catch(err => {
+      console.log("THERE WAS AN ERROR", err);
+      next(err);
+    });
+};
+recommendations.genreMatch = (req, res, next) => {
+  sequelize
+    .query(
+      `SELECT * FROM films WHERE films.genre_id=${
+        res.locals.currentFilm.genre_id
+      }`
+    )
+    .then(films => {
+      res.locals.allMatches = films[0];
+      // console.log('Matching Films',res.locals.allMatches);
+      next();
+    })
+    .catch(err => {
+      console.log("THERE Was an error at GENRE matc", err);
+      next(err);
+    });
+};
+
+recommendations.getReviews = (req, res, next) => {
+  // console.log("Matching Films", res.locals.allMatches);
+  
+    res.locals.reviews = [];
+     res.locals.allMatches.map(match => {
+      axios(
+        `http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${
+          match.id
+        }`
+      ).then(response => {
+        console.log("++++++++++++++CurrRev++++++++++++++:", response.data); // Print the HTML for the Google homepage.
+
+        res.locals.reviews.push(response.data);
+      });
+    });
+
+    next();
+ 
+  
+
+  console.log('LINE 80',res.locals.reviews);
+};
+
+
+
+recommendations.filter = (req, res, next) => {
+  console.log("MOMENT OF TRUTH", res.locals.reviews);
+  next();
+};
+// ROUTES
+app.get(
+  "/films/:id/recommendations",
+  recommendations.currentFilm,
+  recommendations.genreMatch,
+  recommendations.getReviews,
+  recommendations.filter,
+  getFilmRecommendations
+);
+
 const sequelize = new Sequelize("main", null, null, {
   dialect: "sqlite",
   storage: DB_PATH
@@ -39,52 +103,6 @@ const sequelize = new Sequelize("main", null, null, {
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  let filmId = req.params.id;
-
-  sequelize
-    .query(`SELECT * FROM films WHERE films.id=${filmId}`)
-    .then(film => {
-      res.locals.currentFilm = film[0][0];
-      console.log(film);
-    })
-    .then(() => {
-      sequelize
-        .query(
-          `SELECT * FROM films WHERE films.genre_id=${
-            res.locals.currentFilm.genre_id
-          }`
-        )
-        .then(films => {
-          console.log(
-            `+++++++MATCHING GENRE+++++++${JSON.stringify(films[0])}`
-          );
-          res.locals.allMatches = films[0];
-          console.log(
-            `++++RES.LOCALS.currentFilm+++++`,
-            res.locals.allMatches
-          );
-        });
-    })
-    .then(() => {
-      res.locals.allMatches.forEach(match => {
-        request(
-          `http://credentials-api.generalassemb.ly/4576f55f-c427-4cfc-a11c-5bfe914ca6c1?films=${
-            match.id
-          }`,
-          function(error, responce, body) {
-            console.log("body:", body); // Print the HTML for the Google homepage.
-
-            res.locals.allReviews = body;
-            console.log("++++++++REVIEWS++++++++++", res.locals.allReviews);
-          }
-        );
-      });
-    });
-
-  console.log("+++++++++++++++++++ TEST +++++++++++++++++++");
-
-  // console.log(req.params.id);
-
   res.status(500).send("Not Implemented");
 }
 
